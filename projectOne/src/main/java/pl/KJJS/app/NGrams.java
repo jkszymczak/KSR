d208736ca3dff7319e755d882338a90fc97befe2
jkszymczak <kuba_szymcz@wp.pl>
2024-04-09 23:24:35 +0200

    N-Grams done

--- projectOne/src/main/java/pl/KJJS/app/NGrams.java
+++ projectOne/src/main/java/pl/KJJS/app/NGrams.java
@@ -5,27 +5,37 @@ import java.util.List;
 
 public class NGrams {
 
-    double f(double n, double n1, double n2) {
+    public static double f(double n, double n1, double n2) {
 
         return 2 / ((n - n1 + 1) * (n - n1 + 2) - (n - n2 + 1) * (n - n2));
     }
 
     public static String[] splitToSubstring(String s, int n) {
         List<String> substrings = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            if(i+n-1>=s.length()){
-                continue;
-            }
+        for (int i = 0; i < s.length()-n+1; i++) {
+
             substrings.add(s.substring(i,i+n));
         }
         return substrings.toArray(new String[0]);
     }
 
     public static double calculateDistance(String w1, String w2) {
-
+        int n1 = 2;
+        int n2 = 5;
         int n1Length = w1.length();
         int n2Length = w2.length();
-        int nLength = Math.max(n1Length, n2Length);
-        return 0;
+        int n = Math.max(n1Length,n2Length);
+        double fn = f(n,n1,n2);
+        int result =0;
+        for (int i = n1; i <= n2; i++) {
+            String[] substrings = splitToSubstring(w1,i);
+            for (String s:substrings){
+                if(w2.contains(s)){
+
+                    result++;
+                }
+            }
+        }
+        return 1-(result*fn);
     }
 }
--- projectOne/src/test/java/pl/KJJS/app/NGramsTest.java
+++ projectOne/src/test/java/pl/KJJS/app/NGramsTest.java
@@ -1,11 +1,14 @@
 package pl.KJJS.app;
 
+import jdk.internal.vm.annotation.Hidden;
 import junit.framework.TestCase;
 
 public class NGramsTest extends TestCase {
 
     public void testF() {
+        assertEquals(0.056,NGrams.f(7,2,5),0.001);
     }
+
 
     public void testSplitToSubstring() {
         String w = "germany";
@@ -14,11 +17,22 @@ public class NGramsTest extends TestCase {
         for (int i=0;i<results.length;i++){
             assertTrue(results[i].equals(expectedResults[i]));
         }
-
-
-//        assertEquals();
     }
 
+
     public void testCalculateDistance() {
+        String w1 = "canada";
+        String w2 = "japan";
+        double result = NGrams.calculateDistance(w1,w2);
+        assertEquals(result,0.929,0.001);
+    }
+    public void testCommutativity() {
+        String w1 = "canada";
+        String w2 = "japan";
+        assertEquals(NGrams.calculateDistance(w1,w2),NGrams.calculateDistance(w2,w1),0.001);
+    }
+    public void testReturnable(){
+        String w1 = "canada";
+        assertEquals(NGrams.calculateDistance(w1,w1),0.0);
     }
 }
