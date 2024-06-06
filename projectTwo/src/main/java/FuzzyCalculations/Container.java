a8558286da2953e7eb2cc7020d9e2f08e8b68673
jkszymczak <kuba_szymcz@wp.pl>
2024-06-06 17:14:20 +0200

    added ading labels to container

--- projectTwo/src/main/java/FuzzyCalculations/Container.java
+++ projectTwo/src/main/java/FuzzyCalculations/Container.java
@@ -1,5 +1,6 @@
 package FuzzyCalculations;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -7,15 +8,35 @@ import FuzzyCalculations.SummarizerQualifier;
 
 public class Container {
     Map<String, SummarizerQualifier> labels;
+    private Container original = null;
 
     public Container(List<SummarizerQualifier> labels) {
         this.labels = labels.stream().collect(Collectors.toMap(k -> k.getLabel(), v -> v));
+    }
+    public Container(Container c){
+        this.labels = new HashMap<>(c.getLabels());
     }
 
     public SummarizerQualifier getLabel(String label) {
         if (labels.containsKey(label)) return labels.get(label);
         throw new RuntimeException("There is no summarizer or qualifier with label " + label);
     }
+
+    public void addLabel(SummarizerQualifier label){
+        if (original == null){
+            this.original = new Container(this);
+        }
+        this.labels.put(label.getLabel(),label);
+    }
+
+    public Boolean reset(){
+        if(this.original == null) return false;
+        this.labels = this.original.labels;
+        this.original = null;
+        return true;
+    }
+
+
 
     public Map<String, SummarizerQualifier> getLabels() {
         return this.labels;
